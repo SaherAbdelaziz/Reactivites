9d586e69356c0bb232ee04e206bfa8a85e41ca80
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2020-09-06 16:25:37 +0200

    ignore

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,15 @@ namespace Persistence
             
         }
 
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>().HasData(
+                new Value {Id = 1 , Name="First Name"},
+                new Value {Id = 2 , Name="Second Name"},
+                new Value {Id = 3 , Name="Third Name"},
+                new Value {Id = 4 , Name="Foruth Name"}
+            );
+        }
+
         public DbSet<Value> Values { get; set; }
     }
 }
